10d3f1bc605db78555290a836ee038bdb7613339
Stanislav <stanislavpak69020@gmail.com>
2026-05-30 14:58:52 +0500

    fix: replace scanner icon with QR icon in products search button

--- src/pages/InventoryPage.tsx
+++ src/pages/InventoryPage.tsx
@@ -441,8 +441,8 @@ export default function InventoryPage({ branchId, employeeId, role, defaultTab, 
                 onClick={() => setShowScanner(true)}
                 className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-blue-600 text-white rounded-lg"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h1M4 10h1M4 14h1M4 18h1M8 4v16M16 4v16M20 6h-1M20 10h-1M20 14h-1M20 18h-1" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4H7a3 3 0 00-3 3v5M4 16v1a3 3 0 003 3h1M20 12v-1a3 3 0 00-3-3h-1M16 20h1a3 3 0 003-3v-1M8 8h2v2H8zM8 14h2v2H8zM14 8h2v2h-2z" />
                 </svg>
               </button>
             </div>
